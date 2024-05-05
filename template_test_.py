1fc36f81634f3ea85a41f6c7589b21a645ec85bf
elqver <elqver@gmail.com>
2024-05-05 03:11:38 +0300

    better template

--- template_test_.py
+++ template_test_.py
@@ -10,5 +10,7 @@ import pathlib
 def test_main(input_data: bytes, expectation: bytes):
     target = pathlib.Path(__file__).parent.parent.resolve() / pathlib.Path("a.out")
     process = subprocess.Popen(target, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    output_data, _ = process.communicate(input_data)
+    output_data, err = process.communicate(input_data, timeout=1)
+    assert err is None
+    assert process.returncode == 0
     assert output_data.decode().strip() == expectation.decode()
